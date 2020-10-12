62b70c8b9950da12e8f4c67e9ff12d6bb1574b90
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2020-10-12 09:47:29 -0500

    Ya devuelve las filas y columnas de las letras del mensaje

---
+++ program.py
@@ -0,0 +1,31 @@
+#!/bin/sh
+import fileinput
+
+tableau = [['E','N','C','R','Y'],['P','T','A','B','D'],['F','G','H','I','K'],['L','M','O','Q','S'],['U','V','W','X','Z']]
+mensajecif=[]
+###ELIMINAR ESPACIOS EN MENSAJE E INSERTARLO EN UNA LISTA 
+mensaje = 'TRAVEL NORTH AT ONCE'
+mensaje2= list(mensaje.replace(" ",""))
+print(mensaje2)
+indice = 0 
+#for indice, numero in enumerate(mensaje2):
+
+##RECORRER EL TABLEAU E IMPRIMIR NUMERO DE FILA Y COLUMNA 
+fil=0
+col=0
+for indice, numero in enumerate(mensaje2):
+		for fila in tableau:
+			for elemento in fila:
+				#print('elemento[{}][{}]: {} '.format(fil,col,elemento))
+				if mensaje2[indice] == elemento:
+					#print('{} {}\n'.format(fil,col))
+					clave = '{}{}'.format(fil,col)
+					mensajecif.append(clave)
+				col+=1
+			col = 0 
+			fil+=1
+		fil = 0
+
+print(mensajecif)
+
+
