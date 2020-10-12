1e759306c3b439c614371773e77eac78c96538f9
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2020-10-12 16:17:56 -0500

    Se agrego a una funcion el cifrado, se manejara todo por funciones ahora

--- program.py
+++ program.py
@@ -8,52 +8,59 @@ columnas = []
 nuevospares = []
 ###ELIMINAR ESPACIOS EN MENSAJE E INSERTARLO EN UNA LISTA 
 mensaje = 'TRAVEL NORTH AT ONCE'
-mensaje2= list(mensaje.replace(" ",""))
-print(mensaje2)
-indice = 0 
+
 #for indice, numero in enumerate(mensaje2):
 
 ##RECORRER EL TABLEAU E IMPRIMIR NUMERO DE FILA Y COLUMNA 
-fil=0
-col=0
-for indice, numero in enumerate(mensaje2):
-		for fila in tableau:
-			for elemento in fila:
-				#print('elemento[{}][{}]: {} '.format(fil,col,elemento))
-				if mensaje2[indice] == elemento:
-					#print('{} {}\n'.format(fil,col))
-					clave = '{}{}'.format(fil,col)
-					mensajecif.append(clave)
-					filas.append(fil)
-					columnas.append(col)
-					nuevospares = filas + columnas
-				col+=1
-			col = 0 
-			fil+=1
-		fil = 0
 
-print(mensajecif)
-print(filas)
-print(columnas)
-print(nuevospares,'\n\n\n')
-i = 0
-j = 1
-cifrado = ''
-##Formar el mensaje en cifrado 
-for indice, elemento in enumerate(nuevospares):
-	try:
-		print(indice)
-		print('{} {}'.format(nuevospares[i],nuevospares[j]))
-		print('letra cifrada: {} '.format(tableau[nuevospares[i]][nuevospares[j]]))
-		cifrado = cifrado + tableau[nuevospares[i]][nuevospares[j]]
-		print('\n')
-		i+= 2 
-		j += 2
-	except: 
-		print('error')
-		break
+def cifrado(mensaje):
 
-print(cifrado)
+	mensaje2= list(mensaje.replace(" ",""))
+	print(mensaje2)
+	indice = 0 
+	fil=0
+	col=0
+	for indice, numero in enumerate(mensaje2):
+			for fila in tableau:
+				for elemento in fila:
+					#print('elemento[{}][{}]: {} '.format(fil,col,elemento))
+					if mensaje2[indice] == elemento:
+						#print('{} {}\n'.format(fil,col))
+						clave = '{}{}'.format(fil,col)
+						mensajecif.append(clave)
+						filas.append(fil)
+						columnas.append(col)
+						nuevospares = filas + columnas
+					col+=1
+				col = 0 
+				fil+=1
+			fil = 0
+
+	print(mensajecif)
+	print(filas)
+	print(columnas)
+	print(nuevospares,'\n\n\n')
+	i = 0
+	j = 1
+	cifrado = ''
+	##Formar el mensaje en cifrado 
+	for indice, elemento in enumerate(nuevospares):
+		try:
+			print(indice)
+			print('{} {}'.format(nuevospares[i],nuevospares[j]))
+			print('letra cifrada: {} '.format(tableau[nuevospares[i]][nuevospares[j]]))
+			cifrado = cifrado + tableau[nuevospares[i]][nuevospares[j]]
+			print('\n')
+			i+= 2 
+			j += 2
+		except: 
+			print('error')
+			break
+
+	print(cifrado)
+	
+
+cifrado('TRAVEL NORTH AT ONCE')
 
 
 
