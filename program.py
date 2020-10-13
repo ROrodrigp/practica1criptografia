344777c445a3c6aa1e4455132dfa9a776150ad91
Rodrigo Alan García Pérez <rodrigo.agp96@gmail.com>
2020-10-12 19:42:21 -0500

    Se borraron lineas sin usar e impresiones innecesarias

--- program.py
+++ program.py
@@ -7,28 +7,19 @@ filas = []
 columnas = []
 nuevospares = []
 nuevalista = []
-###ELIMINAR ESPACIOS EN MENSAJE E INSERTARLO EN UNA LISTA 
-mensaje = 'TRAVEL NORTH AT ONCE'
 
-#for indice, numero in enumerate(mensaje2):
 
-##RECORRER EL TABLEAU E IMPRIMIR NUMERO DE FILA Y COLUMNA 
 
 def cifrar(mensaje):
-
+	###ELIMINAR ESPACIOS EN MENSAJE E INSERTARLO EN UNA LISTA 
 	mensaje2= list(mensaje.replace(" ",""))
-	#print(mensaje2)
 	indice = 0 
 	fil=0
 	col=0
 	for indice, numero in enumerate(mensaje2):
 			for fila in tableau:
 				for elemento in fila:
-					#print('elemento[{}][{}]: {} '.format(fil,col,elemento))
 					if mensaje2[indice] == elemento:
-						#print('{} {}\n'.format(fil,col))
-						clave = '{}{}'.format(fil,col)
-						mensajecif.append(clave)
 						filas.append(fil)
 						columnas.append(col)
 						nuevospares = filas + columnas
@@ -37,28 +28,20 @@ def cifrar(mensaje):
 				fil+=1
 			fil = 0
 
-	#print(mensajecif)
-	#print(filas)
-	#print(columnas)
-	#print('nuevos pares : \n',nuevospares)
 	i = 0
 	j = 1
 	cifrado = ''
-	##Formar el mensaje en cifrado 
+	##FORMAR EL MENSAJE CIFRADO
 	for indice, elemento in enumerate(nuevospares):
 		try:
-			#print(indice)
-			#print('{} {}'.format(nuevospares[i],nuevospares[j]))
-			#print('letra cifrada: {} '.format(tableau[nuevospares[i]][nuevospares[j]]))
 			cifrado = cifrado + tableau[nuevospares[i]][nuevospares[j]]
-			#print('\n')
 			i+= 2 
 			j += 2
 		except: 
-			#print('error')
 			break
 
 	print(cifrado)
+
 
 def descifrar(cifrado):
 	fil = 0 
@@ -68,7 +51,6 @@ def descifrar(cifrado):
 		for fila in tableau:
 			for elemento in fila: 
 				if cifrado[indice] == elemento:
-					#print('{}{}'.format(fil,col))
 					filas.append(fil)
 					columnas.append(col)
 					nuevospares.append(fil)
@@ -79,15 +61,8 @@ def descifrar(cifrado):
 		fil = 0 
 
 	dividir = int(len(nuevospares) / 2)
-	#print(dividir)
-
 	lista1 = nuevospares[:dividir]
 	lista2 = nuevospares[dividir:]
-
-	#print('\n\n\n')
-	#print(lista1)
-	#print('\n\n\n')
-	#print(lista2)
 
 	for i,e  in enumerate(lista1):
 		nuevalista.append(e)
@@ -96,11 +71,8 @@ def descifrar(cifrado):
 	descifrado = ''
 	for i,e in enumerate(nuevalista):
 		if i % 2 == 0: 
-			#print('\nletra cifrado: {}'.format(tableau[e][nuevalista[i+1]]))
 			descifrado = descifrado + tableau[e][nuevalista[i+1]]
-
-	#print(nuevalista)
-
+			
 	print(descifrado)
 
 
